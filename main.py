06c68f02625cf2fecadbd5668e338d6e1a2d577d
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2023-04-19 14:49:35 +0200

    big update

--- main.py
+++ main.py
@@ -9,73 +9,51 @@
 import os
 import json
 import mne
+import helper
 import numpy as np
 import pandas as pd
 
-import matplotlib.image
-
-import matplotlib.pyplot as plt
-
 #workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+# so execution won't hang when figures are shown
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-# Current path
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
 # Load brainlife config.json
 with open('config.json','r') as config_f:
-    config = json.load(config_f)
+    config = helper.convert_parameters_to_None(json.load(config_f))
 
 # == LOAD DATA ==
-fname = config['fif']
+fname = config['mne']
 raw = mne.io.read_raw_fif(fname, verbose=False)
 
-# eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, n_grad=1, n_mag=1,
-#                                                          n_eeg=1, average=True)
-
-eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, n_grad=config['n_grad'], n_mag=config['n_mag'],
-                                                         n_eeg=config['n_eeg'], average=config['average'])
-
-
-
-# eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'],
-#                                    config['n_grad'], config['n_mag'], config['n_eeg'], config['l_freq'],
-#                                    config['h_freq'], config['average'], config['filter_length'], None,
-#                                    None, None, [], config['avg_ref'], config['no_proj'], config['event_id'],
-#                                    config['eog_l_freq'], config['eog_h_freq'], config['tstart'],
-#                                    config['filter_method'], None, None, config['copy'],
-#                                    config['return_drop_log'], config['meg'])
-
-# eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'], config['n_grad'],
-#             config['n_mag'], config['n_eeg'], config['l_freq'], config['h_freq'], config['average'], config['filter_length'], None, None, None, None, [],
-#             config['avg_ref'], config['no_proj'], config['event_id'], config['ecg_l_freq'], config['ecg_h_freq'], config['tstart'],
-#             config['qrs_threshold'], config['filter_method'], None, config['copy'], config['return_drop_log'], config['meg'])
-
+eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, raw_event=None, tmin=config['tmin'], tmax=config['tmax'], n_grad=config['n_grad'],
+            n_mag=config['n_mag'], n_eeg=config['n_eeg'], l_freq=config['l_freq'], h_freq=config['h_freq'], average=config['average'], 
+            filter_length=config['filter_length'], 
+            n_jobs=-1, ch_name=config['ch_name'], reject=None, flat=None, bads=[],
+            avg_ref=config['avg_ref'], no_proj=config['no_proj'], event_id=config['event_id'], eog_l_freq=config['eog_l_freq'], eog_h_freq=config['eog_h_freq'],
+            tstart=config['tstart'],
+            qrs_threshold=config['qrs_threshold'], filter_method=config['filter_method'], iir_params=config['iir_params'], copy=True, return_drop_log=False,
+            meg=config['meg'])
 
 mne.write_proj('out_dir/proj.fif', eog_projs, overwrite=True)
 
 # == FIGURES ==
-plt.figure(1)
 fig_ep = mne.viz.plot_projs_topomap(eog_projs, info=raw.info)
 fig_ep.savefig(os.path.join('out_figs','eog_projectors.png'))
-
 
 eog_evoked = mne.preprocessing.create_eog_epochs(raw).average()
 eog_evoked.apply_baseline((None, None))
 
-plt.figure(2)
-e=eog_evoked.plot_joint(picks='mag')
-e.savefig(os.path.join('out_figs','meg.png'))
+f = eog_evoked.plot_joint()
+[fig.savefig(os.path.join('out_figs',f'eog_{i}.png')) for i, fig in enumerate(f)]
 
-plt.figure(3)
-e=eog_evoked.plot_joint(picks='grad')
-e.savefig(os.path.join('out_figs','grad.png'))
+report = mne.Report(title='SSP EOG Projectors')
+report.add_projs(info=raw.info, projs=eog_projs, title = 'SSP EOG Projectors')
 
-plt.figure(4)
-e=eog_evoked.plot_joint(picks='eeg')
-e.savefig(os.path.join('out_figs','eeg.png'))
+report.save('out_report/report_ssp.html', overwrite=True)
+
+
+
 
